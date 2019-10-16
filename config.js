2de5711f2793d6a858ffb093df1e33caabc3a588
fengzhijia-source <56635151+fengzhijia-source@users.noreply.github.com>
2019-10-16 19:52:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+// 冯志佳2019/10/16  https://Github.com/AJLoveChina
 var config = {
-    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
         "我最爱的依依",
         "一路风雨走过",
